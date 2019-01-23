6645804ae5df91628fa89a5a5e19bcaa758995b3
Chad Estioco <chadestioco@gmail.com>
2019-01-23 18:36:54 +0800

    Add ray class.

---
+++ src/ray.py
@@ -0,0 +1,18 @@
+from src.vec3 import Vec3
+
+class Ray(object):
+    
+    def __init__(self, a: Vec3, b: Vec3):
+        self.a = a
+        self.b = b
+
+    @property
+    def origin(self):
+        return self.a
+
+    @property
+    def direction(self):
+        return self.b
+
+    def point_at_parameter(t: float) -> Vec3:
+        return self.a + (t * self.b)
