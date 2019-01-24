8106517102ec2b8a91d624be0ddfaed7208591b8
Chad Estioco <chadestioco@gmail.com>
2019-01-24 22:38:43 +0800

    Oh wow my understanding of PPM is wrong and this is proof.

--- src/simple_camera.py
+++ src/simple_camera.py
@@ -18,24 +18,30 @@ if __name__ == "__main__":
     width = 400
     height = 200
     ppm: PPM = PPM(width, height)
-    lower_left_corner: Vec3 = Vec3(-2, -1, -1)
-    h_movement: Vec3 = Vec3(4, 0, 0)
-    v_movement: Vec3 = Vec3(0, 2, 0)
-    origin: Vec3 = Vec3(0, 0, 0)
+    with open("/tmp/simple_camera2.ppm", "w+") as sc2:
+        print(
+            "P3\n%s %s\n255\n" % (width, height),
+            file=sc2
+        )
+        lower_left_corner: Vec3 = Vec3(-2, -1, -1)
+        h_movement: Vec3 = Vec3(4, 0, 0)
+        v_movement: Vec3 = Vec3(0, 2, 0)
+        origin: Vec3 = Vec3(0, 0, 0)
 
-    for j in range(height - 1, -1, -1):
-        for i in range(width):
-            # Get the ratio of how far are we from the "edges". Note that there
-            # is a different definition of an edge, depending on the dimension.
-            u = i / width
-            v = j / height
-            # And use those ratios to "move" the ray away from the origin. Note
-            # that (h_ * u) + (v_ * v) is scaled movement.
-            r: Ray = Ray(origin, lower_left_corner + (h_movement * u) + (v_movement * v))
-            _color: Vec3 = color(r)
-            _color *= 255.9
-            _color.map(int)
+        for j in range(height - 1, -1, -1):
+            for i in range(width):
+                # Get the ratio of how far are we from the "edges". Note that there
+                # is a different definition of an edge, depending on the dimension.
+                u = i / width
+                v = j / height
+                # And use those ratios to "move" the ray away from the origin. Note
+                # that (h_ * u) + (v_ * v) is scaled movement.
+                r: Ray = Ray(origin, lower_left_corner + (h_movement * u) + (v_movement * v))
+                _color: Vec3 = color(r)
+                _color *= 255.9
+                _color.map(int)
 
-            ppm.set_pixel(j, i, _color)
+                ppm.set_pixel(j, i, _color)
+                print("%s %s %s" % _color.make_tuple(), file=sc2)
 
     ppm.write("/tmp/simple_camera.ppm")
