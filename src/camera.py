5582516a3c5ca99ff30fdeefeac69a3cbe6992ae
Chad Estioco <chadestioco@gmail.com>
2019-02-28 21:31:54 +0800

    Add positionable camera. Create camera playground.

--- src/camera.py
+++ src/camera.py
@@ -4,6 +4,8 @@ A pretty hardcoded abstraction for a camera (for now).
 from src.ray import Ray
 from src.vec3 import Vec3
 
+import math
+
 class Camera(object):
 
     def __init__(
@@ -23,4 +25,22 @@ class Camera(object):
         return Ray(
             self.origin,
             self.lower_left_corner + (self.h_movement * u) + (self.v_movement * v) - self.origin
+        )
+
+class PositionableCamera(Camera):
+
+    def __init__(self, vfov: float, aspect_ratio: float):
+        """
+        Create a camera automatically positioned relative to the scene such that
+        it has a certain vertical field-of-view (vfov, expressed in degrees)
+        given the scene's aspect ration.
+        """
+        vfov_rad: float = vfov * math.pi / 180
+        half_height = math.tan(vfov_rad / 2)
+        half_width = aspect_ratio * half_height
+        super().__init__(
+            Vec3(-half_width, -half_height, -1.0),
+            Vec3(2 * half_width, 0, 0),
+            Vec3(0, 2 * half_height, 0),
+            Vec3(0, 0, 0)
         )
---
+++ src/camera_playground.py
@@ -0,0 +1,69 @@
+from src.camera import Camera, PositionableCamera
+from src.hittable import HitRecord, Hittable, HittableList
+from src.material import Lambertian, ReflectionRecord
+from src.ppm import PPM
+from src.ray import Ray
+from src.sphere import Sphere
+from src.utils import _derive_ppm_filename
+from src.vec3 import Vec3
+from typing import List, Optional
+
+import math
+import random
+import sys
+
+UNIT_VEC3: Vec3 = Vec3(1.0, 1.0, 1.0)
+
+def color(ray: Ray, world: HittableList, depth: int) -> Vec3:
+    # Some reflected rays hit not at zero but at some near-zero value due to
+    # floating point shennanigans. So we try to compensate for that.
+    hit_attempt: Optional[HitRecord] = world.hit(ray, 0.001, sys.float_info.max)
+    if hit_attempt is not None:
+        reflection: ReflectionRecord = hit_attempt.material.scatter(ray, hit_attempt)
+        # FIXME Is it really worthwhile to check if reflection is not None here?
+        # All our Materials assume that a hit has been made, and therefore some
+        # reflection should happen (unless it is Vanta).
+        if depth < 50 and reflection is not None:
+            # Compare this with the hard-coded reflection in 6_matterial.
+            return reflection.attenuation * color(reflection.scattering, world, depth + 1)
+        else:
+            return Vec3(0, 0, 0)
+    else:
+        unit_direction: Vec3 = ray.direction.unit_vector()
+        t: float = 0.5 * (unit_direction.y + 1)
+        return ((1.0 - t) * UNIT_VEC3) + (t * Vec3(0.5, 0.7, 1.0))
+
+if __name__ == "__main__":
+    width = 400
+    height = 200
+    sampling_size = 200
+    ppm: PPM = PPM(width, height)
+    cam: Camera = PositionableCamera(90, width / height)
+    radius: float = math.cos(math.pi / 4)
+
+    hittables: List[Hittable] = [
+        Sphere(Vec3(-radius, 0, -1), radius, Lambertian(Vec3(0, 0, 1))),
+        Sphere(Vec3(radius, 0, -1), radius, Lambertian(Vec3(1, 0, 0)))
+    ]
+    world: HittableList = HittableList(hittables)
+
+    for j in range(height - 1, -1, -1):
+        for i in range(width):
+            print("Tracing on row %s, col %s" % (j, i))
+            print("antialiasing...", end="")
+            accumulator: Vec3 = Vec3(0, 0, 0)
+            for sample in range(sampling_size):
+                u: float = float(i + random.random()) / float(width)
+                v: float = float(j + random.random()) / float(height)
+                r: Ray = cam.get_ray(u, v)
+                accumulator += color(r, world, 1)
+
+            accumulator /= float(sampling_size)
+            print("done")
+            accumulator.map(math.sqrt)
+            accumulator *= 255.9
+            accumulator.map(int)
+
+            ppm.set_pixel((height - 1) - j, i, accumulator)
+
+    ppm.write(_derive_ppm_filename())
