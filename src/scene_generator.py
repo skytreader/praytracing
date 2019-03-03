cfb0e7249e8c788abc770eb6b2d8c06953d10ee1
Chad Estioco <chadestioco@gmail.com>
2019-03-04 06:45:25 +0800

    Enter super debug mode.
    The rendering should be fine, the only problem now being the actual
    configuration of the camera and the objects in the scene, as shown by this
    example. Need to figure out the "edges" of our frame with respect to the
    Cartesian coordinate system.

--- src/scene_generator.py
+++ src/scene_generator.py
@@ -22,41 +22,41 @@ def random_scene(
         Sphere(Vec3(0, -1000, 0), 1000, Lambertian(Vec3(0.5, 0.5, 0.5)))
     ]
 
-    for x in range(x_min, x_max):
-        for z in range(z_min, z_max):
-            material_decider: float = random.random()
-            # TODO Clarify what the 0.9 constant here is for.
-            center: Vec3 = Vec3(
-                x + 0.9 * random.random(),
-                0.2,
-                z + 0.9 * random.random()
-            )
+    #for x in range(x_min, x_max):
+    #    for z in range(z_min, z_max):
+    #        material_decider: float = random.random()
+    #        # TODO Clarify what the 0.9 constant here is for.
+    #        center: Vec3 = Vec3(
+    #            x + 0.9 * random.random(),
+    #            0.2,
+    #            z + 0.9 * random.random()
+    #        )
 
-            # TODO What is Vec3(4, 0.2, 0) for?
-            if (center - Vec3(4, 0.2, 0)).length() > 0.9:
-                if material_decider < 0.8:
-                    world.append(
-                        Sphere(center, 0.2, Lambertian(Vec3(
-                            random.random() * random.random(),
-                            random.random() * random.random(),
-                            random.random() * random.random()
-                        )))
-                    )
-                elif material_decider < 0.95:
-                    world.append(
-                        Sphere(center, 0.2, Metal(
-                            Vec3(
-                                0.5 * (1 + random.random()),
-                                0.5 * (1 + random.random()),
-                                0.5 * (1 + random.random())
-                            ),
-                            0.5 * random.random()
-                        ))
-                    )
-                else:
-                    world.append(
-                        Sphere(center, 0.2, Dielectric(1.5))
-                    )
+    #        # TODO What is Vec3(4, 0.2, 0) for?
+    #        if (center - Vec3(4, 0.2, 0)).length() > 0.9:
+    #            if material_decider < 0.8:
+    #                world.append(
+    #                    Sphere(center, 0.2, Lambertian(Vec3(
+    #                        random.random() * random.random(),
+    #                        random.random() * random.random(),
+    #                        random.random() * random.random()
+    #                    )))
+    #                )
+    #            elif material_decider < 0.95:
+    #                world.append(
+    #                    Sphere(center, 0.2, Metal(
+    #                        Vec3(
+    #                            0.5 * (1 + random.random()),
+    #                            0.5 * (1 + random.random()),
+    #                            0.5 * (1 + random.random())
+    #                        ),
+    #                        0.5 * random.random()
+    #                    ))
+    #                )
+    #            else:
+    #                world.append(
+    #                    Sphere(center, 0.2, Dielectric(1.5))
+    #                )
 
     return world
 
@@ -84,7 +84,10 @@ if __name__ == "__main__":
     height: int = 800
     sampling_size: int = 10
     ppm: PPM = PPM(width, height)
-    world = HittableList(random_scene(-11, 11, -11, 11))
+    #world = HittableList(random_scene(-11, 11, -11, 11))
+    world = HittableList([
+        Sphere(Vec3(0, -1000, 0), 1004, Lambertian(Vec3(100, 0.5, 0.5)))
+    ])
 
     lookfrom: Vec3 = Vec3(13, 2, 3)
     lookat: Vec3 = Vec3(0, 0, 0)
